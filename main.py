3eb2ac8682e9fa51a553d92735ac3dbd5ff71564
m1cha1s <msulej04@gmail.com>
2023-02-24 21:07:49 +0100

    Cleaned the code up

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
-import pygame
 import numpy as np
-from typing import Tuple, List
 from random import random
+from typing import List
 from vector import *
+import pygame
+
 
 LINE_SKIP = 10
 MAX_DEPTH = 10
 
 # ASPECT_RATIO = 1/1
-ASPECT_RATIO = 16/9
+ASPECT_RATIO = 16 / 9
 
-WIDTH  = 400
+WIDTH = 400
 HEIGHT = int(WIDTH / ASPECT_RATIO)
 
-SAMPLES_PER_PIXEL = 25;
+SAMPLES_PER_PIXEL = 25
 
-ORIGIN = np.array([0,0,0])
-
-
+ORIGIN = np.array([0, 0, 0])
 
 
 class Ray:
-    def __init__(self, origin: np.ndarray = ORIGIN, direction: np.ndarray = np.array([0,0,0])) -> None:
+    def __init__(self, origin: np.ndarray = ORIGIN, direction: np.ndarray = np.array([0, 0, 0])) -> None:
         self.origin = origin
         self.direction = direction
 
     def at(self, t: float) -> np.ndarray:
-        return self.origin + t*self.direction
+        return self.origin + t * self.direction
 
 
 class HitRecord:
-    def __init__(self, p: np.ndarray = np.array([0,0,0]), normal: np.ndarray = np.array([1,1,1]), t: float = 0, hit: bool = False, frontFace:bool = True) -> None:
+    def __init__(self, p: np.ndarray = np.array([0, 0, 0]), normal: np.ndarray = np.array([1, 1, 1]), t: float = 0,
+                 hit: bool = False, front_face: bool = True) -> None:
         self.p = p
         self.normal = normal
-        self.t = t 
+        self.t = t
         self.hit = hit
-        self.frontFace = frontFace
+        self.frontFace = front_face
 
+    def set_face_normal(self, r: Ray, outward_normal: np.ndarray) -> None:
+        self.frontFace = r.direction.dot(outward_normal) < 0
 
-    def setFaceNormal(self, r: Ray, outwardNormal: np.ndarray) -> None:
-        self.frontFace = r.direction.dot(outwardNormal) < 0
-        
         if self.frontFace:
-            self.normal = outwardNormal
+            self.normal = outward_normal
         else:
-            self.normal = outwardNormal * -1
+            self.normal = outward_normal * -1
 
 
 class Hittable:
@@ -66,8 +65,8 @@ class Sphere(Hittable):
         oc = r.origin - self.center
         a = r.direction.dot(r.direction)
         half_b = oc.dot(r.direction)
-        c = oc.dot(oc) - self.radius*self.radius
-        discriminant = half_b*half_b - a*c
+        c = oc.dot(oc) - self.radius * self.radius
+        discriminant = half_b * half_b - a * c
 
         if discriminant < 0:
             return HitRecord()
@@ -84,12 +83,9 @@ class Sphere(Hittable):
 
         rec = HitRecord(r.at(root), (r.at(root) - self.center) / self.radius, root, True)
 
-        
+        outward_normal = (r.at(root) - self.center) / self.radius
 
-        outwardNormal = (r.at(root) - self.center) / self.radius
-
-        rec.setFaceNormal(r, outwardNormal)
-
+        rec.set_face_normal(r, outward_normal)
 
         return rec
 
@@ -99,67 +95,68 @@ class HittableList(Hittable):
         super().__init__()
         self.objects: List[Hittable] = []
 
-    def append(self, object: Hittable) -> None:
-        self.objects.append(object)
+    def append(self, obj: Hittable) -> None:
+        self.objects.append(obj)
 
     def clear(self) -> None:
         self.objects.clear()
 
     def hit(self, r: Ray, t_min: float, t_max: float) -> HitRecord:
-        closestSoFar = t_max
+        closest_so_far = t_max
 
         rec = HitRecord()
-        for object in self.objects:
-            tmp_rec = object.hit(r, t_min, closestSoFar)
-            if tmp_rec.hit :
-                closestSoFar = tmp_rec.t
+        for obj in self.objects:
+            tmp_rec = obj.hit(r, t_min, closest_so_far)
+            if tmp_rec.hit:
+                closest_so_far = tmp_rec.t
                 rec = tmp_rec
 
         return rec
 
 
 class Camera:
-    def __init__(self, aspectRatio: float) -> None:
+    def __init__(self, aspect_ratio: float) -> None:
         self.viewportHeight = 2
-        self.viewportWidth = self.viewportHeight * aspectRatio
-        self.focalLenght = 1
+        self.viewportWidth = self.viewportHeight * aspect_ratio
+        self.focalLength = 1
 
-        self._origin = np.array([0,0,0])
+        self._origin = np.array([0, 0, 0])
         self._horizontal = np.array([self.viewportWidth, 0, 0])
         self._vertical = np.array([0, self.viewportHeight, 0])
-        self._lowerLeftCorner = self._origin - self._horizontal/2 - self._vertical/2 - np.array([0, 0, self.focalLenght])
+        self._lowerLeftCorner = self._origin - self._horizontal / 2 - self._vertical / 2 - np.array(
+            [0, 0, self.focalLength])
 
-    def getRay(self, u: float, v: float) -> Ray:
-        return Ray(self._origin, self._lowerLeftCorner + u*self._horizontal + v*self._vertical - self._origin)
+    def get_ray(self, u: float, v: float) -> Ray:
+        return Ray(self._origin, self._lowerLeftCorner + u * self._horizontal + v * self._vertical - self._origin)
 
 
-def rayColor(ray: Ray, world: Hittable, depth: int) -> np.ndarray:
+def ray_color(ray: Ray, world: Hittable, depth: int) -> np.ndarray:
     if depth <= 0:
-        return newVector(0,0,0)
+        return newVector(0, 0, 0)
 
     rec = world.hit(ray, 0, np.Infinity)
-    if rec.hit :
+    if rec.hit:
         target = rec.p + rec.normal + randomVectorInUnitSphere()
-        return 0.5 * rayColor(Ray(rec.p, target - rec.p), world, depth -1)
+        return 0.5 * ray_color(Ray(rec.p, target - rec.p), world, depth - 1)
 
     unit_direction = ray.direction / np.linalg.norm(ray.direction)
     t = 0.5 * (unit_direction[1] + 1)
-    color = (1 - t)*np.array([1,1,1]) + t*np.array([0.5, 0.7, 1])
+    color = (1 - t) * np.array([1, 1, 1]) + t * np.array([0.5, 0.7, 1])
     return color
 
 
-def correctColor(color: np.ndarray) -> np.ndarray:
+def correct_color(color: np.ndarray) -> np.ndarray:
     r = color[0]
     g = color[1]
     b = color[2]
 
     scale = 1 / SAMPLES_PER_PIXEL
 
-    r = np.clip(np.sqrt(r * scale),0,0.999)
-    g = np.clip(np.sqrt(g * scale),0,0.999)
-    b = np.clip(np.sqrt(b * scale),0,0.999)
+    r = np.clip(np.sqrt(r * scale), 0, 0.999)
+    g = np.clip(np.sqrt(g * scale), 0, 0.999)
+    b = np.clip(np.sqrt(b * scale), 0, 0.999)
 
-    return np.array([r, g ,b])
+    return np.array([r, g, b])
 
 
 def main():
@@ -176,33 +173,34 @@ def main():
     world.append(Sphere(np.array([0, 0, -1]), 0.5))
     world.append(Sphere(np.array([0, -100.5, -1]), 100))
     # world.append(Sphere(np.array([1, 0, -2]), 0.7))
-    
-    screen.fill((0,0,0))
+
+    screen.fill((0, 0, 0))
     for y in range(HEIGHT):
         for x in range(WIDTH):
 
-            pixelColor = np.array([0,0,0], dtype=float)
-            for _ in range(SAMPLES_PER_PIXEL) :
-                u = (x + random())/(WIDTH-1)
-                v = (y + random())/(HEIGHT-1)
+            pixel_color = np.array([0, 0, 0], dtype=float)
+            for _ in range(SAMPLES_PER_PIXEL):
+                u = (x + random()) / (WIDTH - 1)
+                v = (y + random()) / (HEIGHT - 1)
 
-                r = cam.getRay(u, v)
-                pixelColor += rayColor(r, world, MAX_DEPTH)
+                r = cam.get_ray(u, v)
+                pixel_color += ray_color(r, world, MAX_DEPTH)
 
-            pixelColor = correctColor(pixelColor)
+            pixel_color = correct_color(pixel_color)
 
             # TODO: Potentially flip the axis
-            screen.set_at((WIDTH-x, HEIGHT-y), (pixelColor*255).tolist())
+            screen.set_at((WIDTH - x, HEIGHT - y), (pixel_color * 255).tolist())
 
-        if y % LINE_SKIP == 0 :
+        if y % LINE_SKIP == 0:
             pygame.display.flip()
 
     running = True
-    while running :
+    while running:
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
 
 if __name__ == "__main__":
     main()
